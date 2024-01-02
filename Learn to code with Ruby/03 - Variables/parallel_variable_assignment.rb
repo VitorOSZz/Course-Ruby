da1f35b16a58b7ba89436348e2efeea9074f43d9
VitorOSZz <harlerquin224@gmail.com>
2024-01-02 20:16:55 -0300

    Studded multiples var in one line

---
+++ Learn to code with Ruby/03 - Variables/parallel_variable_assignment.rb
@@ -0,0 +1,8 @@
+=begin
+first_name, middle_name, last_name  = 'Vitor', 'Santos', 'Zorzan'
+
+puts "#{first_name} #{middle_name} #{last_name}"
+=end
+
+real, dollar, euro = 0.20.to_s, 1.to_s, 0.91.to_s
+puts 'real: ' + real , 'dollar: ' + dollar,'euro: ' + euro
---
+++ Learn to code with Ruby/03 - Variables/swapping_variable_values.rb
@@ -0,0 +1,7 @@
+real = false
+fake = true
+
+real, fake = fake, real
+
+puts "real: #{real}"
+puts "fake: #{fake}"
