233eaa29666701144ef901d088e7e602520ca937
VitorOSZz <harlerquin224@gmail.com>
2024-01-05 21:33:52 -0300

    Today I studyng constants

---
+++ Learn to code with Ruby/03 - Variables/constants.rb
@@ -0,0 +1,11 @@
+system('clear')
+puts "Constants is similar a variable, but in constant
+you should'nt change constant like:"
+
+COMPANY_NAME = 'Vitor Company'
+puts COMPANY_NAME
+
+COMPANY_NAME = 'Vitor Company 2'
+puts COMPANY_NAME
+
+puts 'and is a good way to use a constant for performance'
